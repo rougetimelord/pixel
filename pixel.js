1294d464553c645f4a4f56d6664108242d938270
Rouge <sn.falconeye@yahoo.com>
2016-06-10 21:51:05 -0700

    Create basis of project
    Randomly placed and colored pixels will fill the page over time.

---
+++ pixel.js
@@ -0,0 +1,53 @@
+var randoms = {
+    get point() {
+        console.log('New point gotten')
+        return {
+            x: Math.floor(Math.random() * bounds.x + 1), 
+            y: Math.floor(Math.random() * bounds.y + 1)
+        };
+    },
+    get color() {
+        console.log('New color gotten')
+        var tempInt = Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
+        tempInt = (tempInt.length < 6) ? "0".repeat(6 - tempInt.length) + tempInt : tempInt;
+        return '#' + tempInt;
+    }
+}
+var bounds = {
+    get x(){
+        delete this.x;
+        console.log('X gotten and cached.')
+        return this.x = window.innerWidth;
+    },
+    get y(){
+        delete this.y;
+        console.log('Y gotten and cached.')
+        return this.y = window.innerHeight;
+    }
+}
+var create = function(){
+    var coord = randoms.point;
+    var coordId = coord.x + ',' + coord.y;
+    if(document.getElementById(coordId) === null)
+    {
+        var pix = document.createElement('div');
+        pix.id = coordId;
+        pix.className = 'pixel';
+        pix.style.position = 'absolute';
+        pix.style.left = coord.x + 'px';
+        pix.style.top = coord.y + 'px';
+        pix.style.width = '1px';
+        pix.style.height = '1px';
+        pix.style.backgroundColor = randoms.color;
+        document.body.appendChild(pix);
+    }
+    else
+    {
+        document.getElementById(coordId).style.backgroundColor = randoms.color;
+    }
+    console.log('Point generated at %O', coordId);
+    return;
+}
+document.addEventListener('DOMContentLoaded', function () {
+    setInterval(function () { create() }, 25);
+});
